26106ce6dc59deb8531500b6027e1803ff84069f
Heejung Koo <heejung.koo@kellogg.northwestern.edu>
2025-02-21 01:21:04 +0000

    update controller and index

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,18 @@
 class TransactionsController < ApplicationController
   def index
-    render :template => "transactions/index"
+    # render :template => "transactions/index"
+    @transactions = Transaction.all
+    @balances = Hash.new(0)
+
+    @users = User.all
+    for user in @users
+      for transaction in @transactions
+        if transaction["from_user_id"] == user["id"]
+          @balances[user["id"]] = @balances[user["id"]] - transaction["amount"]
+        elsif transaction["to_user_id"] == user["id"]
+          @balances[user["id"]] = @balances[user["id"]] + transaction["amount"]
+        end
+      end
+    end
   end
 end
